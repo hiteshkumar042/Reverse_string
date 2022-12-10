2e9d4b0be963ea277be04a8c0aedb5f1f34c91e2
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-10 09:11:03 +0530

    Added a reverse program using array and loop

---
+++ ReverseString/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word: ");
+            ReverseWord.Reverse(Console.ReadLine());
+        }
+    }
+}
---
+++ ReverseString/ReverseWord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal class ReverseWord
+    {
+        public static void Reverse(String word)
+        {
+            string reverse = "";
+            char[] setOfCharacters = word.ToCharArray();
+            Console.WriteLine("Displaying each character");
+            for (int i = 0; i < setOfCharacters.Length; i++)
+            {
+                Console.Write(setOfCharacters[i] + " ");
+            }
+            for (int i = 0; i < setOfCharacters.Length; i++)
+            {
+                reverse = setOfCharacters[i] + reverse;
+            }
+            Console.WriteLine("\n" + reverse);
+
+            if (word == reverse)
+            {
+                Console.WriteLine("It's a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("It's not a palindrome");
+            }
+        }
+    }
+}
